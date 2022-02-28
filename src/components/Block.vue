326254968cdea8f29563ee1cb1a676910dc014c0
AblueMax <choilamtin2@gmail.com>
2022-02-28 22:39:46 +0800

    updated @ 28/2

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,10 +1,24 @@
 <template>
-  <div class="block">Click Here Now</div>
+  <div class="block" v-if="showBlock">Click Here Now</div>
 </template>
 
 <script>
 export default {
   props: ['delay'],
+  data() {
+    return {
+      showBlock: false,
+    };
+  },
+  mounted() {
+    console.log('mounted');
+    setTimeout(() => {
+      this.showBlock = true;
+    }, this.delay);
+  },
+  updated() {
+    console.log('updated');
+  },
 };
 </script>
 
